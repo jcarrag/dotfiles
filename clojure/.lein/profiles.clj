ad50d437688a5664408099f29e29e40313e35b76
James Carragher <james@carragher.org>
2016-10-21 16:50:05 +0100

    First attempt at atom clojure workflow

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.14.0"]]
-	:dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+	:dependencies [[proto-repl "0.3.1"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
