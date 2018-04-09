6a557073ae9b94d20979b024a3bfc6eed8ff2bd7
James Carragher <james@carragher.org>
2018-04-09 19:21:09 +0900

    update neovim overlay

--- nixpkgs/.config/nixpkgs/overlays/neovim-overlay.nix
+++ nixpkgs/.config/nixpkgs/overlays/neovim-overlay.nix
@@ -2,6 +2,6 @@ self: super:
 
 {
   neovim = super.neovim.override {
-    extraPythonPackages = with self.pythonPackages; [ websocket_client sexpdata ];
+    extraPythonPackages = with self.pythonPackages; [ websocket_client sexpdata neovim ];
   };
 }
