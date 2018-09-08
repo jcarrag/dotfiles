e1c482709b18d8c2b4923f14d2489747d6f0302f
James Carragher <james@carragher.org>
2018-09-08 12:21:35 +0900

    some xmonad keybindings

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,47 @@
-import XMonad
+import System.IO (hClose)
 
-main = xmonad def
-    { terminal    = "urxvt"
-    , modMask     = mod4Mask
+import XMonad
+import XMonad.Hooks.EwmhDesktops (ewmh)
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.EZConfig (mkNamedKeymap)
+import XMonad.Util.NamedActions (NamedAction(..), addDescrKeys, (^++^), subtitle, addName, showKm)
+import XMonad.Util.Run (spawnPipe, hPutStr)
+import System.Taffybar.Hooks.PagerHints (pagerHints)
+
+main = 
+  xmonad
+  $ addDescrKeys ((mod4Mask, xK_F1), showKeybindings) myKeys
+  $ docks
+  $ ewmh
+  $ pagerHints
+  $ myConfig
+  
+myConfig = def
+    { terminal = "kitty"
+    , modMask = mod4Mask
     , borderWidth = 3
+    , startupHook = spawn "taffybar"
     }
+
+myKeys config =
+	 keySet "Audio"
+ 	   [ key "Mute"         "<XF86AudioMute>"        $ spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"
+           , key "Raise Volume" "<XF86AudioRaiseVolume>" $ spawn "pactl set-sink-volume @DEFAULT_SINK@ +5%"
+           , key "Lower Volume" "<XF86AudioLowerVolume>" $ spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%"
+           ]
+	^++^
+   	keySet "Windows"
+           [ key "Kill"             "M-<Backspace>"   $ kill
+           , key "Switch to window" "M-w"             $ spawn "rofi -show window"
+   	   ]
+        where
+   	  keySet s ks = subtitle s : mkNamedKeymap config ks
+   	  key n k a = (k, addName n a)
+
+showKeybindings :: [((KeyMask, KeySym), NamedAction)] -> NamedAction
+showKeybindings x = addName "Show Keybindings" $ io $ do
+  h <- spawnPipe "zenity --text-info --font=terminus"
+  hPutStr h (unlines $ showKm x)
+  hClose h
+  return ()
+
