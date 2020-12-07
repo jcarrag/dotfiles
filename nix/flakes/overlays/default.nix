ba755bae5f1bea2695666d034b4c3e9dee8d9409
James Carragher <james@carragher.org>
2020-12-07 17:54:26 +0000

    remove unused overlays

--- nix/flakes/overlays/default.nix
+++ nix/flakes/overlays/default.nix
@@ -4,10 +4,8 @@ with super.lib;
 let
   overlays = map import [
     ./ferdi.nix
-    ./haskell-language-server.nix
     ./nix-npm-install.nix
     ./parsec.nix
-    ./taffybar.nix
   ];
 in
 foldr (x: y: composeExtensions x y) (self: super: {}) overlays self super
