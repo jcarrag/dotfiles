d3498c1dacc971df72de84ff8d204d9bc70e7617
James Carragher <james@carragher.org>
2021-08-13 15:03:22 +0100

    Add nuc

---
+++ nix/flakes/nixos/nuc/configuration.nix
@@ -0,0 +1,23 @@
+# NUC9i7QNX
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../base-configuration.nix
+    ];
+
+  networking.hostName = "james";
+
+  services = {
+    udev = {
+      # swap left alt with meta on keyboard
+      extraHwdb = ''
+        evdev:input:b0011v0001p0001*
+          KEYBOARD_KEY_db=leftalt
+          KEYBOARD_KEY_38=leftmeta
+      '';
+    };
+  };
+}
