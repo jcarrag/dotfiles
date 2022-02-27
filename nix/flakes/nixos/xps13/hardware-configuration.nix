d4d5289a743f7b2356d67f13fd36a0cc1898d627
James Carragher <james@carragher.org>
2022-02-27 22:51:14 +0000

    remove unnecessary config

--- nix/flakes/nixos/xps13/hardware-configuration.nix
+++ nix/flakes/nixos/xps13/hardware-configuration.nix
@@ -10,7 +10,6 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [];
 
