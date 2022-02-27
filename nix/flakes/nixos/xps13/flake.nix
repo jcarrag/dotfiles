c2171f8b9bb8ac74453baecb1eaa26a6d36326c0
James Carragher <james@carragher.org>
2022-02-27 22:12:02 +0000

    use flake-utils

--- nix/flakes/nixos/xps13/flake.nix
+++ nix/flakes/nixos/xps13/flake.nix
@@ -4,61 +4,29 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   inputs.parsec.url = "github:DarthPJB/parsec-gaming-nix";
 
-  outputs = { self, nixpkgs, unstable, parsec }:
+  outputs = { self, nixpkgs, unstable, flake-utils, parsec }:
     let
-      system = "x86_64-linux";
-
       laptopConfig = import ./configuration.nix;
 
       packageOverlays = import ../../overlays;
 
-      extrasOverlay = _: _: {
-        unstable = import unstable {
-          system = system;
-          config.allowUnfree = true;
-          nixpkgs.overlays = [ packageOverlays ];
-        };
-        colour = "${self}/nix/flakes/nixos/colours/0f111a.png";
-	_self = self;
-      };
-
-      pkgs = import nixpkgs {
-        inherit system; overlays = [ packageOverlays ];
-      };
-
-      # darwinPkgs = import nixpkgs {
-      #   system = "x86_64-darwin";
-      #   overlays = [ extrasOverlay ];
-      # };
-
-      # neovimConfig = import "${self}/nix/flakes/modules/neovim.nix" { pkgs = darwinPkgs; };
-
-      # neovimModuleSrc = import "${nixpkgs}/nixos/modules/programs/neovim.nix" {
-      #   config = neovimConfig;
-      #   lib = darwinPkgs.lib;
-      #   pkgs = darwinPkgs;
-      # };
-
-      # darwinNeovim = (pkgs.lib.debug.traceVal neovimModuleSrc.config.content).programs.neovim.finalPackage;
-      osx = nixpkgs.lib.nixosSystem {
-          system = "x86_64-darwin";
-          modules =
-            [
-              {
-                nixpkgs.overlays = [ extrasOverlay packageOverlays ];
-                nix.registry = {
-                  self.flake = self;
-                  nixpkgs.flake = nixpkgs;
-                  unstable.flake = unstable;
-                };
-              }
-              laptopConfig
-            ];
-        };
-
-      nixos = nixpkgs.lib.nixosSystem {
+      mkNixos = system: 
+        let
+          extrasOverlay = _: _: {
+            unstable = import unstable {
+              system = system;
+              config.allowUnfree = true;
+              nixpkgs.overlays = [ packageOverlays ];
+            };
+            colour = "${self}/nix/flakes/nixos/colours/0f111a.png";
+            _self = self;
+          };
+        in
+          nixpkgs.lib.nixosSystem {
           system = system;
           modules =
             [
@@ -75,9 +43,13 @@
             ];
         };
     in
+    flake-utils.lib.eachDefaultSystem (system:
       {
-        packages.x86_64-darwin.neovim = osx.options.programs.neovim.finalPackage.value;
-        packages.x86_64-linux.neovim = nixos.options.programs.neovim.finalPackage.value;
-        nixosConfigurations.nixos = nixos;
+        packages = flake-utils.lib.flattenTree {
+          neovim = (mkNixos system).options.programs.neovim.finalPackage.value;
+        };
+      }
+      ) // {
+        nixosConfigurations.nixos = mkNixos "x86_64-linux";
       };
 }
