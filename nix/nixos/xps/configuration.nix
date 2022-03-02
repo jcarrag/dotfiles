f5c6c2652736f95eedc39b859a003d755d8fb087
James Carragher <james@carragher.org>
2022-03-02 23:46:23 +0000

    set xrandr defaults for xps

--- nix/nixos/xps/configuration.nix
+++ nix/nixos/xps/configuration.nix
@@ -3,6 +3,24 @@
 
 {
   services = {
+    xserver = {
+      xrandrHeads = [
+        {
+          output = "eDP-1";
+          primary = true;
+          monitorConfig = ''
+            Option "PreferredMode" "1920x1200"
+          '';
+        }
+      ];
+      resolutions = [
+        { x = 2560; y = 1600; }
+        { x = 2048; y = 1280; }
+        { x = 1920; y = 1200; }
+        { x = 1280; y = 800; }
+        { x = 1024; y = 640; }
+      ];
+    };
     udev = {
       # swap left alt with meta on magic keyboard
       extraHwdb = ''
