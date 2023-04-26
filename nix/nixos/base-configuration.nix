2edf1276aac408aff3cd051d6366a2847c4e7558
James Carragher <james@carragher.dev>
2023-04-26 12:09:23 +0100

    add brave mime

--- nix/nixos/base-configuration.nix
+++ nix/nixos/base-configuration.nix
@@ -441,6 +441,14 @@ in
 
   virtualisation.docker.enable = true;
 
+  xdg.mime.defaultApplications = {
+    "text/html" = "brave-browser.desktop";
+    "x-scheme-handler/http" = "brave-browser.desktop";
+    "x-scheme-handler/https" = "brave-browser.desktop";
+    "x-scheme-handler/about" = "brave-browser.desktop";
+    "x-scheme-handler/unknown" = "brave-browser.desktop";
+  };
+
   system.autoUpgrade.enable = true;
   system.stateVersion = "20.09";
 }
