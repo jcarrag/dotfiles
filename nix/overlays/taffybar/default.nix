f2593ab2dd9c2895cadad3f3e45a3c991616e258
James Carragher <james@carragher.dev>
2023-05-19 17:18:38 +0100

    skip checking iptables

--- nix/overlays/taffybar/default.nix
+++ nix/overlays/taffybar/default.nix
@@ -1,15 +1,13 @@
 self: super:
 
 {
-  # taffybar-my = self.haskellPackages.callCabal2nix "taffybar-my" ./. {};
   taffybar-my = self.haskellPackages.developPackage {
     root = ./.;
-    overrides = _: _: {
-      # ghc = self.unstable.haskellPackages.ghc_9_6_1;
-      # ghc.haskellCompilerName = "ghc-9.6.1";
+    overrides = _: _super: {
       # pinned to ghc94 because:
       # https://discourse.haskell.org/t/facing-mmap-4096-bytes-at-nil-cannot-allocate-memory-youre-not-alone/6259
       ghc = self.unstable.haskell.compiler.ghc94;
+      iproute = self.haskell.lib.dontCheck _super.iproute;
     };
   };
 }
