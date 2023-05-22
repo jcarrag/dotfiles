9de4bb967edd251de5c3f9b06a7004c93828c744
James Carragher <james@carragher.dev>
2023-05-22 11:05:32 +0100

    add taffybar/haskell patch

--- nix/modules/taffybar-my.nix
+++ nix/modules/taffybar-my.nix
@@ -11,6 +11,17 @@ in
 
   config = mkIf cfg.enable (
     {
+      # haskell kernel bug: https://discourse.haskell.org/t/facing-mmap-4096-bytes-at-nil-cannot-allocate-memory-youre-not-alone/6259
+      # suggested patch: https://bbs.archlinux.org/viewtopic.php?pid=2100343#p2100343
+      boot.kernelPatches = [{
+        name = "haskell-mmap-fix";
+        patch = builtins.fetchurl
+          {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git/patch/?id=0257d9908d38c0b1669af4bb1bc4dbca1f273fe6";
+            sha256 = "00m8rh121x5bdr07gg9bfj45004f69cp9xhx3fpl88xkj95bc59n";
+          };
+      }];
+
       environment.systemPackages = [
         pkgs.taffybar-my
       ];
