5b0cf2748de0ac4a63d8558f34a517fec5e016ba
James Carragher <james@carragher.dev>
2024-03-19 13:35:51 +0000

    fix path

--- nix/nixos/base-configuration.nix
+++ nix/nixos/base-configuration.nix
@@ -540,7 +540,9 @@ in
       environment = {
         DISPLAY = ":0";
         DBUS_SESSION_BUS_ADDRESS = "unix:path=/run/user/1000/bus";
-        PATH = pkgs.lib.mkForce "PATH:/run/current-system/sw/bin";
+        # /run/wrappers/bin for sudo
+        # /run/current-system/sw/bin for rofi etc.
+        PATH = pkgs.lib.mkForce "PATH:/run/wrappers/bin:/run/current-system/sw/bin";
       };
 
       serviceConfig = {
