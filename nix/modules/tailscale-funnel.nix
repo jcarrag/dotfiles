7aabb07daf9ad868c84874203d59c8395bd377eb
James Carragher <james@carragher.dev>
2024-03-25 16:49:33 +0000

    add calibre-web to hm90

---
+++ nix/modules/tailscale-funnel.nix
@@ -0,0 +1,66 @@
+{ lib, pkgs, config, ... }:
+
+with lib; with lib.types;
+let
+  cfg = config.programs.tailscale-funnel;
+in
+{
+  options.programs.tailscale-funnel = {
+    port = mkOption {
+      type = types.port;
+      default = 443;
+      description = lib.mdDoc "The port at which this Tailscale Funnel is listening.";
+    };
+
+    services = mkOption
+      {
+        description = "The definition of the Tailscale Funnel.";
+        type = types.attrsOf
+          (types.submodule {
+            options = {
+              enable = mkEnableOption "proxying this service via Tailscale Funnel.";
+
+              port = mkOption {
+                type = types.port;
+                description = lib.mdDoc "The port at which this service is listening.";
+              };
+
+              path = mkOption {
+                type = types.str;
+                description = lib.mdDoc "The URL path at which this service will be proxied.";
+              };
+            };
+          });
+      };
+  };
+
+  config =
+    let
+      enabled = lib.attrsets.filterAttrs (n: v: v.enable) cfg.services;
+    in
+    {
+      systemd.services = lib.attrsets.mapAttrs'
+        (name: { enable, port, path }:
+          let
+            start = "${pkgs.unstable.tailscale}/bin/tailscale funnel --bg --https ${builtins.toString cfg.port} --set-path=${path} localhost:${builtins.toString port}";
+            stop = "${start} off";
+          in
+          lib.attrsets.nameValuePair
+            "tailscale-funnel-${name}"
+            {
+              description = "Tailscale Funnel forwarding for ${name}";
+              after = [ "network.target" ];
+              wantedBy = [ "multi-user.target" ];
+
+              serviceConfig = {
+                Type = "oneshot";
+                RemainAfterExit = true;
+                ExecStart = start;
+                ExecStop = stop;
+                Restart = "on-failure";
+              };
+            }
+        )
+        enabled;
+    };
+}
--- nix/nixos/hm90/configuration.nix
+++ nix/nixos/hm90/configuration.nix
@@ -1,10 +1,15 @@
 # EliteMini HM90
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   imports = [
     ../../modules/emby-server.nix
+    ../../modules/tailscale-funnel.nix
   ];
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
 
   programs = {
     emby-server = {
@@ -17,17 +22,33 @@
       enable = true;
       configDir = "/home/james/.config/ynab-updater";
     };
+    tailscale-funnel.services = {
+      calibre-web = {
+        enable = true;
+        port = config.services.calibre-web.listen.port;
+        path = "/";
+      };
+    };
   };
 
   services = {
+    calibre-web = {
+      enable = true;
+      listen.ip = "0.0.0.0";
+      options = {
+        enableBookUploading = true;
+        calibreLibrary = "/home/james/Calibre Library";
+      };
+      user = "james";
+      group = "users";
+    };
     tailscale = {
       enable = true;
+      package = pkgs.unstable.tailscale;
       useRoutingFeatures = "server";
-      package = pkgs.unstable.tailscale.overrideAttrs (old: {
-        postInstall = old.postInstall + ''
-          wrapProgram $out/bin/tailscale --add-flags "--advertise-exit-node"
-        '';
-      });
+      extraUpFlags = [
+        "--advertise-exit-node"
+      ];
     };
     xserver.displayManager.autoLogin = {
       enable = true;
