2ffadde2e57b02dcc6f6563224270e0763cfc658
James Carragher <james@carragher.dev>
2024-03-26 20:08:46 +0000

    autostart sunshine on hm90

--- nix/nixos/hm90/configuration.nix
+++ nix/nixos/hm90/configuration.nix
@@ -55,4 +55,16 @@
       user = "james";
     };
   };
+
+  systemd.user.services.sunshine = {
+    description = "Sunshine server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "exec";
+      ExecStart = "${pkgs.unstable.sunshine}/bin/sunshine";
+      Restart = "on-failure";
+    };
+  };
 }
