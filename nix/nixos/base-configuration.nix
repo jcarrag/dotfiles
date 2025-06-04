7109eee42b1010ffbbb28c3e05f15db5282c9f29
James Carragher <james@carragher.dev>
2025-06-04 10:40:20 +0100

    add osx pub key

--- nix/nixos/base-configuration.nix
+++ nix/nixos/base-configuration.nix
@@ -663,6 +663,7 @@ in
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHMKP2hPhz+L3GJ2eoj4DTtZbdgSm5cS+RVtV9lY7fpB james@carragher.dev" # fwk
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID+kfnnvuaVqRuhUPpPlUY4s7UPMkoI9vGskJxep0ZPa james@carragher.dev" # lunar-fwk
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwJ3qvOGZRCgxKwe9TghG03MyM2eYWLy3wmjVK23T+M james@carragher.dev" # hm90
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPPbut1H1yzwurlRFjIZX/RRxXqMy27jIrccuBX2Fbrb james.carragher@moixa.com" # c07ht12qq6p0
     ];
   };
 
