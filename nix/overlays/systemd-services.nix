dde963446c523afefdb1120c83ef4505e1b59430
James Carragher <james@carragher.dev>
2025-06-07 13:34:29 +0100

    fix syncthing ts binding

--- nix/overlays/systemd-services.nix
+++ nix/overlays/systemd-services.nix
@@ -12,6 +12,18 @@ with self.pkgs; {
           '';
         };
       };
+      # delay syncthing starting until tailscale binds
+      syncthing.serviceConfig.ExecStartPre = lib.mkForce [
+        "${pkgs.bash}/bin/bash -c 'until ${pkgs.iproute2}/bin/ip addr show dev tailscale0 | ${pkgs.gnugrep}/bin/grep -q -E \"inet 100(\.[0-9]{1,3}){3}\"; do sleep 1; done'"
+      ];
+      syncthing.after = lib.mkForce [
+        "network-online.target"
+        "tailscaled.service"
+      ];
+      syncthing.wants = lib.mkForce [
+        "network-online.target"
+        "tailscaled.service"
+      ];
     };
     timers = {
       duck-dns = {
