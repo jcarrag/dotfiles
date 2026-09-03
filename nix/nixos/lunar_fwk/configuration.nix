35e63fe10f4296b4261ab6b613ed8327c59d4908
James Carragher <james@carragher.dev>
2026-09-03 16:02:11 +0100

    fix hyprland-notifier import

--- nix/nixos/lunar_fwk/configuration.nix
+++ nix/nixos/lunar_fwk/configuration.nix
@@ -9,6 +9,7 @@
   imports = [
     ../../modules/immich-camera-sync.nix
     ../../modules/gdrive-sync.nix
+    ../../modules/hyprland-notifier.nix
     ../../modules/sunshine.nix
     ../../modules/tailscale-drive.nix
   ];
